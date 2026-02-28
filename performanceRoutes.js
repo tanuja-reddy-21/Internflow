b6569b9e0b24e95e7c389644060ca5482ed2c2de
Gaddam Tanuja <tanujareddygaddam@gmail.com>
2026-02-28 20:36:58 +0530

    Delete performanceRoutes.js

--- performanceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getPerformance } = require('../controllers/performanceController');
-const { protect } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.get('/:internId', protect, getPerformance);
-
-module.exports = router;
